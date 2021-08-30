04409619583bc29bcf125ead0f76d094079844f5
Colin <colinclissonhg@gmail.com>
2021-08-30 19:45:12 -0400

    hope

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['lh3.googleusercontent.com', 'i.ibb.co', 'cdn.iconscout.com', 'seeklogo.com'],
-  },
-}
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: ["responsive", "group-hover", "hover", "focus", "active"],
-  plugins: [],
-}
