2d0b19b47529be757bf7e4565996dd93ccec6dfd
Colin <colinclissonhg@gmail.com>
2023-01-03 23:21:36 -0500

    Try to remove ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
     head: {
         link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
