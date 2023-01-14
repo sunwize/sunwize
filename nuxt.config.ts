f46a98eda715397e4aa292c3dd6d7fc2a81664f3
Colin <colinclissonhg@gmail.com>
2023-01-14 14:59:10 -0500

    Clear favicon cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     head: {
-        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico?v=2" }],
     },
     css: [
         "~/assets/style/global.scss",
