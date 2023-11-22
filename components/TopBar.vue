48080c807ad6ee5994e4039cb7e0c4a0f60b94c8
Clisson <cclisson@eyeinmedia.com>
2023-11-22 17:01:37 -0500

    feat: improve button for responsiveness

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -22,7 +22,7 @@
             <li class="ml-auto">
                 <a
                     href="/resume.pdf"
-                    class="border border-white hover:bg-white/10 active:bg-white/20 flex items-center gap-1 rounded-3xl px-4 py-2"
+                    class="text-sm border border-white/50 hover:border-white hover:bg-white/10 active:bg-white/20 flex items-center gap-1 rounded-3xl px-4 py-1"
                 >
                     <mdicon name="cloud-download" />
                     My resume
