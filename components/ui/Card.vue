161ef15b2047d4af2f1fcd8e54392192cdb65802
Clisson <cclisson@eyeinmedia.com>
2023-11-30 18:02:51 -0500

    feat: set fixed background color

--- components/ui/Card.vue
+++ components/ui/Card.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <article class="rounded-lg border border-white/10 bg-gray-700/20 shadow-3xl p-3 sm:p-5 md:p-8">
+    <article class="rounded-lg border border-white/10 bg-[#151b22] shadow-3xl p-3 sm:p-5 md:p-8">
         <slot />
     </article>
 </template>
